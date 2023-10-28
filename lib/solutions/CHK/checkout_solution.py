63db9828a27657f771924551a3fbec2a017b05cc
sbx_user1051 <sbx_user1051@169.254.35.185>
2023-10-28 15:22:59 +0000

    Sat Oct 28 15:22:59 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,9 +2,9 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-itemprice = {'A':50, 'B':30, 'C':20, 'D':15, 'E': 40, 'F': 10}
-multibuy = {'A':[(5,200),(3,130)], 'B':[(2,45)]}
-multifree = {'E': ('B', 2, 1), 'F':('F',2,1)}
+itemprice = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90, 'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50, 'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90, 'Y': 10, 'Z': 50}
+multifree = {'E':('B',2,1),'F':('F',2,1),'N':('M',3,1), 'R':('Q',3,1), 'U':('U',3,1)}
+multibuy = {'A':[(5,200),(3,130)], 'B':[(2,45)], 'H':[[10,80],[5,45]], 'K':[[2,150]], 'P':[[5,200]], 'Q':[[3,80]], 'V':[[3,130],[2,90]]}
 
 def checkout(skus):
     totalprice = 0
@@ -62,9 +62,9 @@ def checkout(skus):
 
 
 def test():
-    items = 'FFFFFF'
+    items = 'FFFFFFRQ'
     price = 40
     total = checkout(items)
     print(price==total, price, total)
 
-#test()
+test()
