19b37983a7c5bbe39245a5e5753564d26d8135e4
sbx_user1051 <sbx_user1051@169.254.35.185>
2023-10-28 14:21:21 +0000

    Sat Oct 28 14:21:21 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 itemprice = {'A':50, 'B':30, 'C':20, 'D':15}
-multibuy = {'A':[3,130], 'B':[2,45]}
+multibuy = {'A':[3,20], 'B':[2,15]}
 
 def checkout(skus):
+    sumprice = 0
+    multibuy_count = {}
     for item in sku:
-        if item in itemprice:
-            # check item exists
+        if item in itemprice: # check item exists
+            sumprice += itemprice[item] # add price to sum
+            if item in multibuy: # process discount
+                if item in multibuy_count:
+                    multibuy_count[item] += 1
+                else:
+                    multibuy_count[item] = 1
+
+            
+        else:
+            sumprice = -1
     
+
