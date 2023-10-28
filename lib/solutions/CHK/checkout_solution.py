d64596c4f27b4a30f74be8bcabeac78cfef8248a
sbx_user1051 <sbx_user1051@169.254.35.185>
2023-10-28 14:18:21 +0000

    Sat Oct 28 14:18:21 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,8 +2,11 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-itemprice = {'A':50, 'B':}
+itemprice = {'A':50, 'B':30, 'C':20, 'D':15}
+multibuy = {'A':[3,130], 'B':[2,45]}
 
 def checkout(skus):
+    for item in sku:
+        if item in itemprice:
+            # check item exists
     
-
