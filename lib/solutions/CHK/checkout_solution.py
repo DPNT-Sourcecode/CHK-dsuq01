a4b3f997a54397a56f4ff70d61db5899eb8367fb
sbx_user1051 <sbx_user1051@169.254.35.185>
2023-10-28 15:02:38 +0000

    Sat Oct 28 15:02:38 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 itemprice = {'A':50, 'B':30, 'C':20, 'D':15, 'E': 40}
-multibuy = {'A':[[5,200],[3,130]], 'B':[[2,45]]}
+multibuy = {'A':[(5,200),(3,130)], 'B':[(2,45)]}
 multifree = {'E': ['B', 2, 1]}
 
 def checkout(skus):
     totalprice = 0
     itemcount = {} # count the number of every different item, so that when I deduct free items, the count does not go beyond 0
-    #multifree_count = {}
-    #multibuy_count = {}
     itemlist = []
     for item in skus:
         if item in itemprice: # check item exists
@@ -40,9 +38,7 @@ def checkout(skus):
     for item in itemlist:
         if item in multibuy:
             cond = multibuy[item]
-            for c in cond:
-                num = c[0]
-                discount = c[1]
+            for (num,discount) in cond:
                 offernum = itemcount[item] // num
                 totalprice += offernum * discount
                 itemcount[item] -= offernum * num
@@ -55,10 +51,10 @@ def checkout(skus):
 
 
 def test():
-    items = 'AAAAAAAABBBCDE'
+    items = 'E'
     price = 200+130+45+30+20+15+40
     total = checkout(items)
     print(price==total, price, total)
 
-#test()
+test()
 
